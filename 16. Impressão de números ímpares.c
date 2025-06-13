065901139a11f94c7e0d9571e9af4656e9da70ff
franduda2006 <85133471+franduda2006@users.noreply.github.com>
2025-06-13 00:12:48 -0300

    arrumando o código da questestão 16. Impressão de números ímpares

--- 16. Impressão de números ímpares.c
+++ 16. Impressão de números ímpares.c
@@ -1,29 +1,11 @@
 #include <stdio.h>
 
-void imprimirImparesRecursivo(int n, int atual) {
-    if (atual > n) {
-        return;
-    }
-
-    printf("%d ", atual);
-    imprimirImparesRecursivo(n, atual + 2);
-}
-
-void imprimirImpares(int n) {
-    if (n >= 1) {
-        imprimirImparesRecursivo(n, 1);
-    }
-}
-
-#include <stdio.h>
-
 void imprimirImpares(int n, int atual) {
     if (atual > n) {
         return;
     }
 
     printf("%d ", atual);
-
     imprimirImpares(n, atual + 2);
 }
 
@@ -33,9 +15,8 @@ int main() {
     scanf("%d", &num);
 
     printf("Números ímpares de 1 até %d:\n", num);
-    
     imprimirImpares(num, 1);
-
     printf("\n");
+
     return 0;
 }
